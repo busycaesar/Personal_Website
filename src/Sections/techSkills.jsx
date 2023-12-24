54d66a11af8742e1c9b7bd2abc4b402ba5e908d6
Dev Shah <busycaesar@gmail.com>
2023-12-24 14:41:36 -0500

    Uniformed the size of the tech logos!

--- src/Sections/techSkills.jsx
+++ src/Sections/techSkills.jsx
@@ -38,48 +38,48 @@ export default function TechSkills() {
           { name: "JavaScript", width: "50", src: JavaScript },
           { name: "C#", width: "60", src: CSharp },
           { name: "Python", width: "55", src: Python },
-          { name: "HTML", width: "50", src: HTML },
-          { name: "CSS", width: "75", src: CSS },
-          { name: "C++", width: "52", src: CPP },
-          { name: "C", width: "53", src: C },
+          { name: "HTML", width: "48", src: HTML },
+          { name: "CSS", width: "70", src: CSS },
+          { name: "C++", width: "48", src: CPP },
+          { name: "C", width: "49", src: C },
         ]}
       />
       <TechSkillsSection
         title="Libraries, Tools and Frameworks"
         techs={[
           { name: "Node.js", width: "50", src: NodeJS },
-          { name: "Express.js", width: "60", src: ExpressJS },
-          { name: "Next.js", width: "63", src: NextJS },
-          { name: "React.js", width: "60", src: ReactJS },
-          { name: "JWT", width: "55", src: JWT },
-          { name: "Bootstrap", width: "70", src: Bootstrap },
+          { name: "Express.js", width: "55", src: ExpressJS },
+          { name: "Next.js", width: "57", src: NextJS },
+          { name: "React.js", width: "55", src: ReactJS },
+          { name: "JWT", width: "50", src: JWT },
+          { name: "Bootstrap", width: "65", src: Bootstrap },
         ]}
       />
       <TechSkillsSection
         title="Database"
         techs={[
-          { name: "MongoDB", width: "70", src: MongoDB },
-          { name: "PostgreSQL", width: "70", src: Postgressql },
+          { name: "MongoDB", width: "60", src: MongoDB },
+          { name: "PostgreSQL", width: "55", src: Postgressql },
         ]}
       />
       <TechSkillsSection
         title="Version Control System"
         techs={[
-          { name: "Git", width: "60", src: Git },
-          { name: "GitHub", width: "60", src: GitHub },
-          { name: "Node Package Manager", width: "85", src: NPM },
+          { name: "Git", width: "55", src: Git },
+          { name: "GitHub", width: "55", src: GitHub },
+          { name: "Node Package Manager", width: "70", src: NPM },
         ]}
       />
       <TechSkillsSection
         title="Software Application"
         techs={[
-          { name: "VS Code", width: "50", src: VSCode },
-          { name: "Postman", width: "55", src: Postman },
-          { name: "MS SQL Server", width: "70", src: MSSQL },
-          { name: "Visual Paradigm", width: "50", src: VisualParadigm },
-          { name: "Figma", width: "35", src: Figma },
-          { name: "MS Project", width: "50", src: MSProject },
-          { name: "Discord", width: "58", src: Discord },
+          { name: "VS Code", width: "47", src: VSCode },
+          { name: "Postman", width: "53", src: Postman },
+          { name: "MS SQL Server", width: "60", src: MSSQL },
+          { name: "Visual Paradigm", width: "47", src: VisualParadigm },
+          { name: "Figma", width: "33", src: Figma },
+          { name: "MS Project", width: "47", src: MSProject },
+          { name: "Discord", width: "55", src: Discord },
         ]}
       />
     </div>
