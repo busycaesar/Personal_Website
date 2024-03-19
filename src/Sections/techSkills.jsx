51945f58b49cdbdcf467b26ec8d1b58c7279a8e8
Dev Shah <busycaesar@gmail.com>
2024-03-19 15:41:03 -0400

    Refactor: Migrate on premise photos to skillicons

--- src/Sections/techSkills.jsx
+++ src/Sections/techSkills.jsx
@@ -1,31 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
-import {
-  Bootstrap,
-  C,
-  CPP,
-  CSharp,
-  JavaScript,
-  Python,
-  HTML,
-  CSS,
-  NodeJS,
-  ExpressJS,
-  ReactJS,
-  NextJS,
-  MongoDB,
-  Postgressql,
-  Git,
-  GitHub,
-  VSCode,
-  Postman,
-  MSSQL,
-  Figma,
-  VisualParadigm,
-  MSProject,
-  Discord,
-  NPM,
-  JWT,
-} from "@/Photos";
 import TechSkillsSection from "@/Components/techSkillsSection";
 
 export default function TechSkills() {
@@ -35,53 +9,75 @@ export default function TechSkills() {
         <strong>TECHNICAL SKILLS</strong>
       </h1>
       <TechSkillsSection
-        title="Programming Languages"
+        title="Languages"
         techs={[
-          { name: "JavaScript", width: "50", src: JavaScript },
-          { name: "C#", width: "60", src: CSharp },
-          { name: "Python", width: "55", src: Python },
-          { name: "HTML", width: "48", src: HTML },
-          { name: "CSS", width: "70", src: CSS },
-          { name: "C++", width: "48", src: CPP },
-          { name: "C", width: "49", src: C },
+          { name: "JavaScript", src: "js" },
+          { name: "C#", src: "cs" },
+          { name: "Python", src: "python" },
+          { name: "HTML", src: "html" },
+          { name: "CSS", src: "css" },
+          { name: "C++", src: "cpp" },
+          { name: "C", src: "c" },
         ]}
       />
       <TechSkillsSection
-        title="Libraries, Tools and Frameworks"
+        title="Technologies"
         techs={[
-          { name: "Node.js", width: "50", src: NodeJS },
-          { name: "Express.js", width: "55", src: ExpressJS },
-          { name: "Next.js", width: "57", src: NextJS },
-          { name: "React.js", width: "55", src: ReactJS },
-          { name: "JWT", width: "50", src: JWT },
-          { name: "Bootstrap", width: "65", src: Bootstrap },
+          { name: "Node.js", src: "nodejs" },
+          { name: "Express.js", src: "expressjs" },
+          { name: "React.js", src: "react" },
+          { name: "Next.js", src: "nextjs" },
+          { name: "Vite", src: "vite" },
+          { name: "Docker", src: "docker" },
+          { name: "Jest", src: "jest" },
+          { name: "Tailwindcss", src: "tailwindcss" },
+          { name: "Bootstrap", src: "bootstrap" },
+          { name: "MaterialUI", src: "materialui" },
         ]}
       />
       <TechSkillsSection
         title="Database"
         techs={[
-          { name: "MongoDB", width: "60", src: MongoDB },
-          { name: "PostgreSQL", width: "55", src: Postgressql },
+          { name: "MongoDB", src: "mongodb" },
+          { name: "PostGres", src: "postgres" },
+          { name: "Firebase", src: "firebase" },
         ]}
       />
       <TechSkillsSection
         title="Version Control System"
         techs={[
-          { name: "Git", width: "55", src: Git },
-          { name: "GitHub", width: "55", src: GitHub },
-          { name: "Node Package Manager", width: "70", src: NPM },
+          { name: "Git", src: "git" },
+          { name: "GitHub", src: "github" },
+          { name: "GitHub Actions", src: "githubactions" },
+          { name: "Node Package Manager", src: "npm" },
         ]}
       />
       <TechSkillsSection
         title="Software Application"
         techs={[
-          { name: "VS Code", width: "47", src: VSCode },
-          { name: "Postman", width: "53", src: Postman },
-          { name: "MS SQL Server", width: "60", src: MSSQL },
-          { name: "Visual Paradigm", width: "47", src: VisualParadigm },
-          { name: "Figma", width: "33", src: Figma },
-          { name: "MS Project", width: "47", src: MSProject },
-          { name: "Discord", width: "55", src: Discord },
+          { name: "VS Code", src: "vscode" },
+          { name: "VS Community", src: "visualstudio" },
+          { name: "Eclipse", src: "eclipse" },
+          { name: "Postman", src: "postman" },
+          { name: "Vercel", src: "vercel" },
+          { name: "Figma", src: "figma" },
+          { name: "Discord", src: "discord" },
+        ]}
+      />
+      <TechSkillsSection
+        title="Operating Systems"
+        techs={[
+          { name: "Linux", src: "linux" },
+          { name: "Windows", src: "windows" },
+        ]}
+      />
+      <TechSkillsSection
+        title="Currently Learning"
+        techs={[
+          { name: "BASH", src: "bash" },
+          { name: "VIM", src: "vim" },
+          { name: "AWS", src: "aws" },
+          { name: "Java", src: "java" },
         ]}
       />
     </div>
